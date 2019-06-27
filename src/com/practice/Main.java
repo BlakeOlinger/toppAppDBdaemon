196ec3f41656576fd2cb7030c024633b9524a26b
Blake Olinger <blake.olinger@symbosoft.com>
2019-06-27 12:48:19 -0400

    Initial commit

---
+++ src/com/practice/Main.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+// this daemon is designed to interface between git repository db and
+// the app install directory to automate git functions and
+// later automate app updates
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
