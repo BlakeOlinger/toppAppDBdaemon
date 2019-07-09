5e93063f067ea27672d2b04f2ede68c6ee57ddc9
Blake Olinger <blake.olinger@symbosoft.com>
2019-07-09 10:47:39 -0400

    -- added .bat for windows starter file - fixes all issues with master starting everything

---
+++ programFiles/ToppApp.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cd "C:\Users\bolinger\Desktop\test install\"
+
+start toppAppMaster.jar
+
+start toppApp.jar
+
+start toppAppDBdaemon.jar
+
+start toppAppUpdater.jar
+
+cd "C:\Users\bolinger\Desktop\test install\programFiles\sw\"
+
+start sw-part-auto-test.exe
+
+exit
